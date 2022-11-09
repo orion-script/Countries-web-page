27e297a910dd9fb520c6bdba36f016fd47704d2a
Olaniyi John Ayomide <jaolaniyi96@student.lautech.edu.ng>
2022-11-09 18:17:01 +0100

    fix some styles

--- src/components/CountryCards.jsx
+++ src/components/CountryCards.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 function CountryCards({ country }) {
   return (
     <Link
-      className="bg-white w-4/5 md:w-auto m-auto h-96 rounded-xl shadow-xl dark:bg-gray-700 dark:text-white"
+      className="bg-white w-4/5 md:w-auto m-auto h-auto rounded-xl shadow-xl dark:bg-gray-700 dark:text-white pb-5"
       to="/details"
       state={country}
     >
       <img
         src={country.flags.svg}
         alt={country.flags}
-        className="rounded-t-xl"
+        className=" h-46 rounded-t-xl"
       />
       <div className="pl-5">
         <h1 className="font-bold text-lg md:text-xl py-3">
