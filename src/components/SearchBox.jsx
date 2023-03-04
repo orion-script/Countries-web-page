b53f22cf7bfcb734bfb709b090b0bfd96f43e8af
Olaniyi John Ayomide <jaolaniyi96@student.lautech.edu.ng>
2023-03-04 12:46:43 +0100

    change the search box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,13 +2,30 @@ import React from "react";
 
 function SearchBox({ onChangeHandler, placeholder, searchField }) {
   return (
-    <div className="w-11/12 m-auto md:m-0 md:w-2/5">
+    <div className="relative w-11/12 m-auto md:m-0 md:w-2/5">
+      <span className="absolute inset-y-0 left-0 flex items-center py-4 w-11/12 m-auto md:m-0 md:w-2/5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="ml-3 w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </span>
       <input
         type="search"
+        name="Search"
         value={searchField}
-        className="h-14 w-full rounded-lg shadow-lg pl-4 md:pl-5 bg-white dark:bg-gray-700 dark:text-white"
         placeholder={placeholder}
         onChange={onChangeHandler}
+        className="h-14 w-full rounded-lg shadow-lg pl-[50px] md:pl-10 bg-white dark:bg-gray-700 dark:text-white focus:outline-none"
       />
     </div>
   );
